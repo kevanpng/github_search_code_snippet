2567be3fb408b8736db5c6ba43beeaf569a40df9
Kevan Png <kevanpngyeowwei@gmail.com>
2019-11-02 17:57:26 +0800

    make async version of request using aiohttp. timeit says that it is 2x
    faster for 40 results.

--- main_async.py
+++ main_async.py
@@ -1,13 +1,21 @@
 from secrets import ACCESS_TOKEN
 import timeit
 
-import requests
+
 from github import Github
+
+import aiohttp
+import asyncio
 
 g = Github(ACCESS_TOKEN)
 
 
-def search_github(keyword):
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+
+async def search_github(session, keyword):
     rate_limit = g.get_rate_limit()
     rate = rate_limit.search
     if rate.remaining == 0:
@@ -21,23 +29,24 @@ def search_github(keyword):
 
     # queries only python files
     query = f'"{keyword}" in:file extension:py'
-    result = g.search_code(query, order='desc')
+    results = g.search_code(query, order='desc')
 
     max_size = 40
-    print(f'Found {result.totalCount} file(s)')
+    print(f'Found {results.totalCount} file(s)')
     print(f'printing first {max_size} files')
-    if result.totalCount > max_size:
-        result = result[:max_size]
+    if results.totalCount > max_size:
+        results = results[:max_size]
 
     download_urls = []
-    for file in result:
-        download_urls.append(file.download_url)
+    for result in results:
+        download_urls.append(result.download_url)
     print(download_urls)
     print('downloading content')
     path = '/home/kevan/Desktop/github_dirs/github_search_code_snippet/tmp/github_search_results.txt'
     with open(path, 'w+') as f:
         for url in download_urls:
-            content = requests.get(url).text
+            content = await fetch(session, url)
+            # content = requests.get(url).text
             f.write(content)
             f.write('---------------------------')
             f.write('\n')
@@ -45,9 +54,16 @@ def search_github(keyword):
             # time.sleep(1)
 
 
+async def main():
+    async with aiohttp.ClientSession() as session:
+        await search_github(session, keyword)
+        # html = await fetch(session, 'http://python.org')
+
+
 if __name__ == '__main__':
     keyword = input('Enter keyword[e.g french, german etc]: ')
     start_time = timeit.default_timer()
-    search_github(keyword)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
     elapsed = timeit.default_timer() - start_time
     print(elapsed)
